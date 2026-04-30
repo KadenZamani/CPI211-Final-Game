b5cb6d63bcabbaa4c03cdee780bd5b414969e275
KadenZamani <kazamani@asu.edu>
2026-04-30 13:14:06 -0700

    added bite sound

--- Final Game/Assets/Scripts/RxAttackVariable.cs
+++ Final Game/Assets/Scripts/RxAttackVariable.cs
@@ -6,6 +6,9 @@ public class RxAttackVariable : MonoBehaviour
     public bool b = false;
     private bool e = false;
     public Animator rexAnim;
+    public AudioSource rexSound;
+    public AudioClip roarSound;
+    public AudioClip BiteSound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,4 +34,15 @@ public class RxAttackVariable : MonoBehaviour
     {
         a = false;
     }
+
+    void PlayRoarSound()
+    {
+        rexSound.PlayOneShot(roarSound );
+    }
+
+    void PlayBiteSound()
+        {
+            rexSound.PlayOneShot(BiteSound);
+    }   
+
 }

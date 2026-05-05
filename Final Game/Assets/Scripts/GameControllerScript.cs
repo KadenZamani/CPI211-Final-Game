62aeb84addc4fbef89981e132aab810b4c94170b
TristanZamani <tzamani@asu.edu>
2026-05-04 21:28:39 -0700

    Update GameControllerScript.cs

--- Final Game/Assets/Scripts/GameControllerScript.cs
+++ Final Game/Assets/Scripts/GameControllerScript.cs
@@ -10,12 +10,7 @@ public class GameControllerScript : MonoBehaviour
     [SerializeField] public bool key1 = false;
     [SerializeField] public bool key2 = false;
     [SerializeField] public bool key3 = false;
-   // [SerializeField] private EnemyStateController miniboss1;
-  //  [SerializeField] private EnemyStateController miniboss2;
-  //  [SerializeField] private EnemyStateController miniboss3;
-   // [SerializeField] private GameObject minibossObject1;
-   // [SerializeField] private GameObject minibossObject2;
-  //  [SerializeField] private GameObject minibossObject3;
+
 
     public static GameControllerScript Instance;
 
